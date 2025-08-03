d5cea0c7a5bd431de982d243af89db55bd8eeaab
naito hayato <naitohayato1998@gmail.com>
2025-08-03 12:23:18 +0900

    変数⇒プリミティブ型と参照型

--- JavaProject/neco-simple-java-project/src/naitoupra/naitou_blackjack/App.java
+++ JavaProject/neco-simple-java-project/src/naitoupra/naitou_blackjack/App.java
@@ -78,20 +78,62 @@ public class App {
         // }
         // }
 
-        for (int i = 1; i <= 10; i++) {
-            int mod3 = i % 3;
-            int mod2 = i % 2;
-            int mod5 = i % 5;
+        // for (int i = 1; i <= 10; i++) {
+        // int mod3 = i % 3;
+        // int mod2 = i % 2;
+        // int mod5 = i % 5;
 
-            if (mod3 == 0) {
-                System.out.println(i + "は、３の倍数です");
-            } else if (mod2 == 0) {
-                System.out.println(i + "は、２の倍数です");
-            } else if (mod5 == 0) {
-                System.out.println(i + "は、５の倍数です");
-            } else {
-                System.out.println(i + "は、２と３と５の倍数ではありません");
-            }
+        // if (mod3 == 0) {
+        // System.out.println(i + "は、３の倍数です");
+        // } else if (mod2 == 0) {
+        // System.out.println(i + "は、２の倍数です");
+        // } else if (mod5 == 0) {
+        // System.out.println(i + "は、５の倍数です");
+        // } else {
+        // System.out.println(i + "は、２と３と５の倍数ではありません");
+        // }
+        // }
+        // }
+        // }
+
+        // int[] points = new int[7];
+        // // [xx xx xx xx xx] ←イメージ
+        // // [0 1 2 3 4] ←インデックス（xx番目）
+
+        // points[0] = 67;
+        // points[1] = 38;
+        // points[2] = 78;
+        // points[3] = 57;
+        // points[4] = 94;
+        // points[5] = 75;
+        // points[6] = 20;
+        // // [67,38,78,57,94]
+        // // System.out.println(points[3]);
+
+        // int arraysize = points.length;
+
+        // for (int i = 0; i < arraysize; i++) {
+        // System.out.println(points[i]);
+        // }
+        // リストの宣言
+        // int[] points = new int[5];これに相当
+        List<Integer> array = new ArrayList<>();// いくらでも要素は追加できる：サイズ指定の必要はない。
+
+        // [xx xx xx xx xx ...]←イメージ
+        // [0 1 2 3 4 ...]←インデックス
+
+        // 要素の追加 points[0] = 67；←これに相当
+        // 0番目から順に入っていく。
+        array.add(67);
+        array.add(38);
+        array.add(78);
+        array.add(57);
+        array.add(94);
+        // array.add(2,78);// インデックスも取得できる（関連：オーバーロード）
+        // 要素の取得
+        int arraySize = array.get(0);// ラッパークラスなので、int型でも受け取れる。
+        for (int i = 0; i < arraySize; i++) {
+            System.out.println(array.get(i));
         }
     }
 }
